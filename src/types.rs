ebaa7d251b7b41a0536857ce052d164ea1269b63
KAIYOHUGO <41114603+KAIYOHUGO@users.noreply.github.com>
2022-05-31 02:42:56 +0000

    ⚒️ change snake_case

--- src/types.rs
+++ src/types.rs
@@ -1,11 +1,11 @@
-use crate::token::{Span};
+use crate::token::Span;
 
 #[cfg(feature = "serde")]
 use serde::Serialize;
 
 #[non_exhaustive]
 #[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(Serialize))]
+#[cfg_attr(feature = "serde", derive(Serialize), serde(rename_all = "snake_case"))]
 pub enum Block {
     Paragraph(BlockParagraph),
     Heading(BlockHeading),
@@ -13,7 +13,7 @@ pub enum Block {
 
 #[non_exhaustive]
 #[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(Serialize))]
+#[cfg_attr(feature = "serde", derive(Serialize), serde(rename_all = "snake_case"))]
 pub enum Inline {
     Inlines(Vec<Inline>),
     Bold(InlineBold),
@@ -24,20 +24,20 @@ pub enum Inline {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(Serialize))]
+#[cfg_attr(feature = "serde", derive(Serialize), serde(rename_all = "snake_case"))]
 pub struct File {
     pub content: Vec<Block>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(Serialize))]
+#[cfg_attr(feature = "serde", derive(Serialize), serde(rename_all = "snake_case"))]
 pub struct BlockParagraph {
     pub span: Span,
     pub content: Inline,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(Serialize))]
+#[cfg_attr(feature = "serde", derive(Serialize), serde(rename_all = "snake_case"))]
 pub struct BlockHeading {
     pub span: Span,
     pub level: usize,
@@ -45,35 +45,35 @@ pub struct BlockHeading {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(Serialize))]
+#[cfg_attr(feature = "serde", derive(Serialize), serde(rename_all = "snake_case"))]
 pub struct InlineBold {
     pub span: Span,
     pub content: Box<Inline>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(Serialize))]
+#[cfg_attr(feature = "serde", derive(Serialize), serde(rename_all = "snake_case"))]
 pub struct InlineItalic {
     pub span: Span,
     pub content: Box<Inline>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(Serialize))]
+#[cfg_attr(feature = "serde", derive(Serialize), serde(rename_all = "snake_case"))]
 pub struct InlineUnderline {
     pub span: Span,
     pub content: Box<Inline>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(Serialize))]
+#[cfg_attr(feature = "serde", derive(Serialize), serde(rename_all = "snake_case"))]
 pub struct InlineLinethrough {
     pub span: Span,
     pub content: Box<Inline>,
 }
 
 #[derive(Debug, Clone, PartialEq)]
-#[cfg_attr(feature = "serde", derive(Serialize))]
+#[cfg_attr(feature = "serde", derive(Serialize), serde(rename_all = "snake_case"))]
 pub struct Text {
     pub span: Span,
     pub content: String,
