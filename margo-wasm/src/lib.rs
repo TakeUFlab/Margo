7da02613bc6b9abf4fb5782964a2fe9f6429707e
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2022-06-07 21:09:44 +0800

    🔕 Rename

--- margo-wasm/src/lib.rs
+++ margo-wasm/src/lib.rs
@@ -2,7 +2,7 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn parse(content: String) -> Result<JsValue, JsError> {
-    let tokenstream = margo::parse(content).unwrap();
+    let tokenstream = margo::parse_string(content).unwrap();
     let js = serde_wasm_bindgen::to_value(&tokenstream)?;
     Ok(js)
 }
